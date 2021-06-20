098d99553322dbe4eb23fc99b8a42e92460e9c53
Marwan <marwang11@gmail.com>
2021-06-20 11:52:26 +0300

    2.7 Practice Project with Collections in Java

---
+++ src/main/collectionAssisted.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Vector;
+
+/**
+ *
+ * @author m-w-n
+ */
+public class collectionAssisted
+{
+    public static void main(String[] args)
+    {
+        //creating arraylist
+        System.out.println("ArrayList");
+        ArrayList<String> city=new ArrayList<String>();
+        city.add("Bangalore");
+        city.add("Delhi");
+        System.out.println(city);
+        
+        //creating vector
+        System.out.println("\n");
+        System.out.println("Vector");
+        Vector<Integer> vec = new Vector();
+        vec.addElement(15);
+        vec.addElement(30);
+        System.out.println(vec);
+        
+        //Creating linkedList
+        
+        System.out.println("\n");
+        System.out.println("LinkedList");
+        LinkedList<String> names = new LinkedList<String>();
+        names.add("Alex");
+        names.add("John");
+        Iterator<String>itr = names.iterator();
+        while (itr.hasNext())
+        {
+            System.out.println(itr.next());
+        }
+            //creating hashset
+            System.out.println("\n");
+            System.out.println("HashSet");
+            HashSet<Integer>set = new HashSet<Integer>();
+            set.add(101);
+            set.add(103);
+            set.add(102);
+            set.add(104);
+            System.out.println(set);
+            
+            //Creating linkedHashSet
+            System.out.println("\n");
+            System.out.println("LinkedHashSet");
+            LinkedHashSet<Integer> set2 = new LinkedHashSet<Integer>();
+            set2.add(11);
+            set2.add(13);
+            set2.add(12);
+            set2.add(14);
+            System.out.println(set2);
+    }
+}
